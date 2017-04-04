a64002487f338a5274dbee02c57de3e378fdd804
Kirill Kondratenko <kk92@ya.ru>
2017-04-04 14:04:08 +0300

    add

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from functools import partial
-import logging.config
+import logging.handlers
+import logging
 
 import arrow
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Unicode, Integer, Index, text, CheckConstraint
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy_utils import ArrowType
 
-log = logging.getLogger(__name__)
 Column = partial(Column, nullable=False)
-ArrowType = partial(ArrowType, timezone=True)
-
 NO_WHITESPACE_REGEX = r'^\S*$'
 
 app = Flask(__name__)
 app.config.from_object('settings')
-logging.config.dictConfig(app.config['LOGGER_CONFIG'])
+
+handler = logging.handlers.RotatingFileHandler(
+    app.config['LOG_FILE'], maxBytes=10 * 1024000, backupCount=10, encoding='utf-8')
+handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname).1s] %(message)s'))
+handler.setLevel(logging.DEBUG)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
+
 cors = CORS(app, resources={'*': {'origins': '*'}})
 db = SQLAlchemy(app)
 
@@ -35,13 +41,43 @@ class Participant(db.Model, PrimaryKeyMixin):
     employees = Column(Unicode)
     selected_prizes = Column(Unicode)
 
-    added = Column(ArrowType(), default=arrow.utcnow)
-    subscribed_to_newsletter = Column(ArrowType(), nullable=True)
+    added = Column(ArrowType(timezone=True), default=arrow.utcnow)
+    subscribed_to_newsletter = Column(ArrowType(timezone=True), nullable=True)
 
     __table_args__ = (
-        CheckConstraint("email ~ '{}'".format(NO_WHITESPACE_REGEX)),
+        CheckConstraint("email ~ '{}' AND length(email) > 4".format(NO_WHITESPACE_REGEX)),
         Index('index_unique_lowercase_email', text('lower(email)'), unique=True),
     )
+
+
+def add_participant(data):
+    def get(key): return data.get(key, '').strip()
+
+    try:
+        participant = Participant(
+            name=get('name'),
+            phone=get('tel'),
+            email=get('email'),
+            occupation=get('occupation'),
+            employees=get('workers'),
+            selected_prizes=get('prizes').strip(';'),
+        )
+        db.session.add(participant)
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+        app.logger.error(e)
+    except Exception:
+        db.session.rollback()
+        raise
+
+
+@app.route('/add', methods=['POST'])
+def show_add_participant():
+    app.logger.debug('Incoming POST request: {}'.format(request.form))
+    add_participant(request.form)
+
+    return ''
 
 
 if __name__ == '__main__':
--- settings_sample.py
+++ settings_sample.py
@@ -1,34 +1,9 @@
 import os.path
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://crazykit:password@localhost:5432/crazykit'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://crazykit:password@localhost:5432/crazykit'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SECRET_KEY = 'secret key must be secret'
-
-LOGGER_NAME = 'crazykit'
+SECRET_KEY = 'Must be secret'
 
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
-LOGGER_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname).1s] %(message)s',
-        },
-    },
-    'handlers': {
-        'logfile': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(PROJECT_DIR, 'logs', 'app.log'),
-            'formatter': 'standard',
-            'maxBytes': 10 * 1024000,
-            'backupCount': 10,
-        },
-    },
-    'loggers': {
-        LOGGER_NAME: {
-            'handlers': ['logfile'],
-            'level': 'DEBUG',
-        },
-    }
-}
+LOG_FILE = os.path.join(PROJECT_DIR, 'logs', 'app.log')
